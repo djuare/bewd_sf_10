49d83b5e523faaae1fbbf5ec692119668a00a1b5
djuare <djuare@gmail.com>
2015-08-05 18:48:05 -0700

    lesson two notes

---
+++ 02_Ruby_Fundamentals_1/exercises/array_work.rb
@@ -0,0 +1,35 @@
+#Array_work
+
+country = ['USA', 'Cananda', 'Spain', 'Mexico', 'Germany', 'Japan']
+puts "The size of my array is #{country.size}"
+puts "My country array is equal to: #{country}"
+puts ''
+
+puts "I removed Japan from the array #{country.pop} "
+puts "Now The size of my array is #{country.size}"
+puts "My country array is equal to: #{country}"
+
+puts ''
+#puts "Original array is #{country}"
+
+#puts "Added Italy #{country.unshift}"
+
+#puts "Original array is #{country}"
+
+#var3 = country.unshift("Italy")
+#puts var3
+
+#var4 = country << 'France'
+#puts var4
+
+#var5 = country.push('Belgium', 'France', 'Spain')
+#puts var5
+
+#var6 = country.unshift
+#puts var6
+
+#var7 = country.length
+#puts var7
+
+#var8 = country << 'China' 'Korea'
+#puts var8
