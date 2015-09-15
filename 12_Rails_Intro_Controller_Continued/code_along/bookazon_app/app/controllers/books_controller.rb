d07c15ae4b7678282ff08076cbd56a1a77834247
djuare <djuare@gmail.com>
2015-09-15 04:50:41 -0700

    12_Rais_Intro_Controller_notes - DJ

--- 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
+++ 12_Rails_Intro_Controller_Continued/code_along/bookazon_app/app/controllers/books_controller.rb
@@ -6,18 +6,30 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @book = get_book
+
   end
 
   def update
+    @books = get_book
+    if @book.update(book_params)
   end
 
   def destroy
@@ -26,5 +38,10 @@ class BooksController < ApplicationController
   private
 
   def book_params
+    params.require(:book).permit(:title, :year_published, :author, :available, :genre, :image_url)
+  end
+
+  def get_book
+    @book = Book.find(params[:id])
   end
 end
